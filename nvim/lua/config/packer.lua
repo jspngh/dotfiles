4df39a5eb5974f7f9853f27f53c5d07f29485e02
Jonas Spanoghe <jonas@spanoghe.net>
2023-01-25 14:42:28 +0100

    nvim: fix *cmp* dependencies

--- nvim/lua/config/packer.lua
+++ nvim/lua/config/packer.lua
@@ -39,6 +39,8 @@ return require('packer').startup(function(use)
   use('neovim/nvim-lspconfig')
   use('hrsh7th/cmp-nvim-lsp')
   use('hrsh7th/nvim-cmp')
+  use('hrsh7th/cmp-vsnip')
+  use('hrsh7th/vim-vsnip')
   use('hrsh7th/cmp-buffer')
   use('hrsh7th/cmp-path')
 
