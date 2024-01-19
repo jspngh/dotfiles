90867f37f4fab0e80767da677e437d5c568c7db9
Jonas Spanoghe <jonas.spanoghe@atlascopco.com>
2024-01-19 16:32:34 +0100

    nvim: move nvim-cmp configuration

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -1,13 +1,107 @@
 return {
     "hrsh7th/nvim-cmp",
-    -- load cmp on InsertEnter
-    event = "InsertEnter",
+    event = { "InsertEnter", "CmdlineEnter" },
     dependencies = {
         "hrsh7th/cmp-nvim-lsp",
         "hrsh7th/cmp-buffer",
         "hrsh7th/cmp-path",
     },
     config = function()
-        -- ...
+        local cmp = require('cmp')
+        cmp.setup({
+            mapping = {
+                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                ['<C-Space>'] = cmp.mapping.complete(),
+                ['<C-e>'] = cmp.mapping.close(),
+                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                ['<Tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item()
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+            },
+            sources = cmp.config.sources({
+                { name = 'nvim_lsp', max_item_count = 8 },
+            }, {
+                { name = 'buffer', max_item_count = 4 },
+            }),
+            completion = {
+                keyword_length = 4
+            }
+        })
+
+        -- Setup lspconfig.
+        local nvim_lsp = require('lspconfig')
+        local capabilities = require('cmp_nvim_lsp').default_capabilities()
+        local on_attach = function(client, bufnr)
+            -- Enable completion triggered by <c-x><c-o>
+            vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+
+            -- Mappings.
+            -- See `:help vim.lsp.*` for documentation on any of the below functions
+            local bufopts = { noremap=true, silent=true, buffer=bufnr }
+            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+            vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+            vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+            vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+            vim.keymap.set('n', '<space>wl', function()
+                print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+            end, bufopts)
+            vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+            vim.keymap.set('n', '<space>cr', vim.lsp.buf.rename, bufopts)
+            vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+            vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+            vim.keymap.set('n', '<space>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+        end
+
+        nvim_lsp["clangd"].setup({
+            on_attach = on_attach,
+            flags = {
+                debounce_text_changes = 150,
+            },
+            cmd = {
+                "clangd",
+                "-j=3",
+                "--compile-commands-dir=build",
+                "--background-index",
+                "--clang-tidy",
+                "--completion-style=detailed",
+                "--header-insertion=never"
+            },
+            capabilities = capabilities,
+            root_dir = function()
+                return vim.fn.getcwd()
+            end
+        })
+
+        nvim_lsp["rust_analyzer"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            flags = {
+                debounce_text_changes = 150,
+            },
+            settings = {
+                ["rust-analyzer"] = {
+                    checkOnSave = {
+                        allTargets = false
+                    }
+                }
+            }
+        })
+
+        nvim_lsp["pyright"].setup({
+            on_attach = on_attach,
+            capabilities = capabilities,
+            root_dir = nvim_lsp.util.find_git_ancestor,
+            flags = {
+                debounce_text_changes = 150,
+            }
+        })
     end,
 }
