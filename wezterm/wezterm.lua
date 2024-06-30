8f28656a3464f0c73d258073edbe745af7c0d246
Jonas Spanoghe <jonas@spanoghe.net>
2024-06-30 18:28:19 +0200

    wezterm: update config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,37 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'MaterialDark'
 config.font_size = 18
 config.font = wezterm.font 'Berkeley Mono'
+
+config.initial_rows = 36
+config.initial_cols = 100
+
+config.color_scheme = 'MaterialDark'
+config.colors = {
+    tab_bar = {
+        inactive_tab = {
+            bg_color = '#3e3e3e',
+            fg_color = '#808080',
+        },
+        active_tab = {
+            bg_color = '#181c24',
+            fg_color = '#d0d0d0',
+        },
+    }
+}
+
 config.window_padding = {
   left = 0,
   right = 0,
   top = 0,
   bottom = 0,
 }
+
+config.window_frame = {
+    font_size = 14,
+}
+
+config.hide_tab_bar_if_only_one_tab = true
 
 return config
