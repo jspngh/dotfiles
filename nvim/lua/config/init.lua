e4810a071e65f77c1ca23e10849767f9f7e2045c
Jonas Spanoghe <jonas@spanoghe.net>
2025-05-08 12:19:04 +0200

    nvim: visualize tabs

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -12,6 +12,14 @@ vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.list = true
+vim.opt.listchars:append {
+    tab = "│─",
+    multispace = " ",
+    lead = " ",
+    trail = "·",
+    nbsp = "·"
+}
 
 vim.opt.smartindent = true
 
