d0e9c836ea656dc46eb1ebbcd559f70da4ac96b6
Jonas Spanoghe <jonas@spanoghe.net>
2026-06-04 22:27:50 +0200

    nvim: fix treesitter highlights

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,11 +3,11 @@ return {
     lazy = false,
     build = ":TSUpdate",
     config = function ()
-      require("nvim-treesitter").setup {
-          ensure_installed = { "c", "lua", "rust", "json" },
-          auto_install = true,
-          sync_install = false,
-          highlight = { enable = true },
-      }
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = { "c", "lua", "rust", "json", "markdown" },
+        callback = function(args)
+          vim.treesitter.start(args.buf)
+        end,
+      })
     end
 }
